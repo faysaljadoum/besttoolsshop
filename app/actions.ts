4b83af2f85ecdb33fd5b891061f1aaf8ada69eb7
faysal jadoum <faycalrachid3@gmail.com>
2026-01-18 01:04:58 +0100

    change

--- app/actions.ts
+++ app/actions.ts
@@ -19,7 +19,6 @@ export async function submitOrder(formData: FormData) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        // Astuce importante : Vercel gère parfois mal les retours à la ligne (\n) dans les variables
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       },
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
