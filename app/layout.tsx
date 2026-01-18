187ac44db78c9c9d83af7e203d2e84a3e64ab234
faysal jadoum <faycalrachid3@gmail.com>
2026-01-18 15:51:45 +0100

    add pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,11 @@ import { Cairo } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Analytics } from "@vercel/analytics/next"
+import Script from 'next/script'
+import FacebookPageView from "./components/FacebookPageView ";
 
 const cairo = Cairo({ subsets: ["arabic"] });
+const PIXEL_ID = process.env.FACEBOOK_PIXEL_ID
 
 export const metadata: Metadata = {
   title: "Best tools ",
@@ -23,6 +26,29 @@ export default function RootLayout({
         <Navbar />        
         {children}
         <Analytics />
+
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        <FacebookPageView />
+
+
       
       </body>
     </html>
